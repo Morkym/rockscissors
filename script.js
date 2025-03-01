6c57f742d54e44355a90728fb0d1f672e9951170
Morkym <morcharia@gmail.com>
2025-03-01 07:50:55 +0300

    ui2

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let humanScore = 0;
 let computerScore = 0;
-let counter = 1;
+let counter = 0;
+let roundResult;
+let rounds;
+
 function getComputerChoice(){
    const compChoice = Math.floor(Math.random()*3);
    console.log(compChoice)
@@ -15,6 +18,8 @@ document.addEventListener("DOMContentLoaded", function(event){
     const paper =document.querySelector("#paper");
     const scissors =document.querySelector("#scissors");
     const reset= document.querySelector("#reset");
+    roundResult = document.querySelector("#displayResults");
+    rounds = document.querySelector("#rounds");
     if(rock && paper&& scissors && reset){
         rock.addEventListener("click", function(){
             getHumanChoice(0);
@@ -46,67 +51,87 @@ console.log("computer choice is: " + getComputerChoice());
 
 
 function playRound(){
+    
     if (counter >=5){
         console.log("Game over")
+        
+        results()
         gameReset()
     }
     const computerChoice = getComputerChoice();
         
     console.log(computerChoice)
     console.log(humanChoice)
+    let resultMessage = "";
+    
 
 
     if (humanChoice === 0 && computerChoice ===1){
-    console.log("paper beats rocks you lose hommie");
+    resultMessage="paper beats rocks you lose  this round hommie";
     computerScore++;
     }
     else if(humanChoice===0 && computerChoice===2){
-    console.log("You win ! Rocks beat scissors");
+    resultMessage="You win this round ! Rocks beat scissors";
     humanScore++;
     }    
     else if(humanChoice===1 && computerChoice===2){
-    console.log("You lose!Scissors beat paper");
+    resultMessage="You lose this round!Scissors beat paper";
     computerScore++;
     }
     else if(humanChoice===1 && computerChoice===0){
-    console.log("You win!Paper beat rocks");
+    resultMessage="You win this round!Paper beat rocks";
     humanScore++;
     }
     else if(humanChoice===2 && computerChoice===0){
-    console.log("You lose ! Rocks beat scissors");
+    resultMessage="You lose this round ! Rocks beat scissors";
     computerScore++; 
     } 
     else if(humanChoice===2 && computerChoice===1){
-    console.log("You win ! scissors beat paper");
+    resultMessage="You win this round ! scissors beat paper";
     humanScore++;
     }
     else if (computerChoice===humanChoice){
-     console.log("oops! it's a tie");
+     resultMessage="oops! that was a tie";
     }
     else{
     console.log("wrong entry")
     }
     counter++;
     console.log("round " + counter)
-if (counter===5){
-    results()
-}
+    roundResult.innerHTML = resultMessage;
+    rounds.innerHTML = "Round " + counter;
+    if (counter === 5){
+        results()
+    
+    }
+    
 }
 
+
 function results(){
+let gameResult = "";
 if (humanScore > computerScore){
-    console.log("You win the game after 5 rounds")
+    gameResult ="You win the game after 5 rounds,you scored: " + humanScore + "<br>Game over! try again";
+        console.log("You win the game after 5 rounds")
 }
 else if (humanScore < computerScore){
+    gameResult ="You lose the game after 5 rounds <br>you scored, " + humanScore + " <br>Game over! try again";
     console.log("You lose the game after 5 rounds")
 }
 else{
-    console.log("It's a tie")
+    gameResult="It's a tie <br>your score is " + humanScore +  "<br>Game over! try again" ;
+
 }
 console.log("Your score is " + humanScore)
+roundResult.innerHTML = gameResult;
+gameReset()
 }function gameReset(){
     humanScore = 0;
     computerScore = 0;
-    counter = 1;
+    counter = 0;
     console.log("reset")
+    //roundResult.innerHTML = "Game over,Press any button to try again";
+    rounds.innerHTML = "Round " + counter;
+    
+    
 }
